138b41ee17d0296ecafa73670f2f8a2040037271
Mayank Mahadevwala <mmah.axelor@gmail.com>
2017-01-23 11:21:25 +0530

    feat() : add Player class - a person plays teenpattie - just take name field

---
+++ src/main/java/com/axelor/teenpattie/model/Player.java
@@ -0,0 +1,18 @@
+package com.axelor.teenpattie.model;
+
+public class Player {
+	private String name;
+	
+	public Player(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
