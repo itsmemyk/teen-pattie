20fc275fe2eb21d44124720a6f63fe1cd5d53f9f
Mayank Mahadevwala <mmah.axelor@gmail.com>
2017-01-23 11:20:51 +0530

    feat() : add Card Class (RANK + SUIT) - integrate rank and suit - assign dual values to each card using static

---
+++ src/main/java/com/axelor/teenpattie/model/Card.java
@@ -0,0 +1,54 @@
+package com.axelor.teenpattie.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class Card {
+	private SUIT suit;
+	private RANK rank;
+	private static int[] rankDualValues = new int[14];
+	
+	static {
+		initDuals();
+	}
+	
+	private static void initDuals() {
+		int c = 1;
+		int n = 2;
+		while (c <= RANK.values().length) {
+			rankDualValues[c] = n;
+			c++;
+			n = n * 2;
+		}
+		
+	}
+	
+	public Card(SUIT suit, RANK rank) {
+		this.suit = suit;
+		this.rank = rank;
+	}
+
+	public SUIT getSuit() {
+		return suit;
+	}
+
+	public void setSuit(SUIT suit) {
+		this.suit = suit;
+	}
+
+	public RANK getRank() {
+		return rank;
+	}
+
+	public void setRank(RANK rank) {
+		this.rank = rank;
+	}
+	
+	public String toString() {
+		return this.suit.getSymbol() + this.rank.getIdentity();
+	}
+	
+	public int getDualValue() {
+		return rankDualValues[this.rank.getValue()-1];
+	}
+}
